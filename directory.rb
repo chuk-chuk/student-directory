be336db508e32659a223e685ebcdc75b0b90f6cd
chuk-chuk <yulia.v.litvin@gmail.com>
2017-06-19 18:38:56 +0000

    used an array to print names by iteration referring to each value individually

--- directory.rb
+++ directory.rb
@@ -1,18 +1,23 @@
-student_count = 11
-#first we print the list of students
+#let's put all students into the array
+students = [
+  "Dr. Hannibal Lector",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+#and then print them
 puts "The students of Villians Academy"
 puts "______________"
-puts "Dr. Hannibal Lector"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts student
+end 
 #finally, we print the total number of students
-print "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
 #it's important that print() doesn't add new line characters
