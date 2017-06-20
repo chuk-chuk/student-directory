cd6967fd936cb62b4721147496623862326f4ea0
chuk-chuk <yulia.v.litvin@gmail.com>
2017-06-20 12:39:38 +0000

    added a new method input_students that asks for input from the user and keeps adding names into the array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,34 @@
-#let's put all students into the array
-students = [
-  {name: "Dr. Hannibal Lector", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name:"The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# students = [
+#   {name: "Dr. Hannibal Lector", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name:"The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+#]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = Array.new
+  #get the first name
+  name = gets.chomp
+  #while the name isn't empty repaet this code
+  while !name.empty? do
+    #add a student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  #return an arrray of students
+  students
+end
+
 def print_header
   puts "The students of Villians Academy"
   puts "______________"
@@ -27,6 +44,8 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
+#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
